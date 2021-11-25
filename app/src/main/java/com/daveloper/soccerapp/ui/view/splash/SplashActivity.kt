423a5226896840a6be01c4a01f55db51ef1e3e50
daveloper32 <80218906+daveloper32@users.noreply.github.com>
2021-11-25 09:41:34 -0500

    Initial commit, starting project, adding splash, colors, app icon, permissions and Gradle config

---
+++ app/src/main/java/com/daveloper/soccerapp/ui/view/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.daveloper.soccerapp.ui.view.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.daveloper.soccerapp.auxiliar.ext_fun.goToXActivity
+import com.daveloper.soccerapp.ui.view.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        goToXActivity(MainActivity::class.java)
+    }
+}
